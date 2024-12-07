d64222df1330579732b79a3adb1ac8d3d8f6f67c
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2024-12-08 00:51:43 +0800

    Updated struct to include json name

--- server-side/services/user/models/user-model.go
+++ server-side/services/user/models/user-model.go
@@ -1,10 +1,10 @@
 package models
 
 type User struct {
-	userID         int
-	Name           string
-	Email          string
-	contactNo      int
-	hashedPassword string
-	membershipTier string
+	UserID         int    `json:"user_id"`
+	Name           string `json:"name"`
+	Email          string `json:"email"`
+	ContactNo      string `json:"contact_no"`
+	HashedPassword string `json:"hashed_password"`
+	MembershipTier string `json:"membership_tier"`
 }
